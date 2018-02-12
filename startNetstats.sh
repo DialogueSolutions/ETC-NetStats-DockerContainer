b9e477e968e377ab1de2742c3556f3b3a3b0a2d7
dsnode <miken@dialoguesolutions.net>
2018-02-11 21:12:46 -0500

    Version 2 working still need autoconfig for running container

---
+++ startNetstats.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+#If you know what you're doing feel free to write your custom start geth commands here
+
+#If you don't wish to use pm2 to start netstats
+#pm2 start /.ethereum-classic/process.json
+/bin/bash
